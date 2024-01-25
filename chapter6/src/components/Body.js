d417b72775c8ec03cfe0cd706692104f9fe98582
tejashwatiwari <tejashwatiwari@gmail.com>
2024-01-24 21:58:54 -0800

    chapter6 - Integrated API

--- chapter6/src/components/Body.js
+++ chapter6/src/components/Body.js
@@ -1,15 +1,33 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockdata";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+
 
 const Body = () => {
   //State Variable - Super Powerful Variable
   //We will use Hooks - using React State
   //Hook is a normal JS function - its prebuilt - its a utility function
   //useState() or useEffect() - it maintains the state of the component
-  const [listOfRestaurants, setListOfRestaurant] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurant] = useState([]);
 
-    return (
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+    const fetchData = async () => {
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.2101585&lng=77.4293184&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+      const json = await data.json();
+      console.log(json);
+      setListOfRestaurant(json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    };
+
+    // if (listOfRestaurants.length===0) {
+    //   return <Shimmer />
+    // }
+
+    return listOfRestaurants.length===0?(<Shimmer />):(
       <div className="body">
         <div className="filter">
           <button className="filter-btn" onClick={() => {      
--- chapter6/src/components/Header.js
+++ chapter6/src/components/Header.js
@@ -1,11 +1,14 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 
 const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+  console.log("Header render");
     return (
       <div className="header">
         <div className="logo-container">
-          <img className="logo" alt = "logo" src={LOGO_URL} />
+          <img className="logo" src="https://pngfre.com/wp-content/uploads/Burger-45.png" />
         </div>
         <div className="nav-items">
             <ul>
@@ -13,11 +16,20 @@ const Header = () => {
               <li>About Us</li>
               <li>Contact Us</li>
               <li>Cart</li>
+              <button 
+              className="login" 
+              onClick={() => {
+                btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Logout");
+              }}
+              >
+                {btnNameReact}
+              </button>
             </ul>
         </div>
-  
       </div>
-    )
+    );
   };
 
   export default Header
