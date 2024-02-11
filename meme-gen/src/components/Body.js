953ebd4d52684c94f67145ac2e0c6664fc0ddb0c
tejashwatiwari <tejashwatiwari@gmail.com>
2024-02-11 02:03:44 -0800

    Cleanup

--- meme-gen/src/components/Body.js
+++ meme-gen/src/components/Body.js
@@ -16,11 +16,11 @@ const Body = () => {
 
     const fetchData = async () => {
       const data = await fetch(
-        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.2101585&lng=77.4293184&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        "https://api.imgflip.com/get_memes"
         );
       const json = await data.json();
-      console.log(json);
-      setListOfRestaurant(json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      setListOfRestaurant(json?.data?.memes);
+      console.log(setListOfRestaurant(json?.data))
     };
 
     // if (listOfRestaurants.length===0) {
@@ -47,7 +47,7 @@ const Body = () => {
         </div>
         <div className="res-container">
         {
-          listOfRestaurants.map((restaurant) => <RestaurantCard key= {restaurant.info.id} resData={restaurant}/>)
+          listOfRestaurants.map((meme) => <RestaurantCard key= {meme.id} resData={meme}/>)
         }
         </div>
       </div>
---
+++ meme-gen/src/components/RestaurantCard.js
@@ -0,0 +1,22 @@
+import {CDN_URL} from "../utils/constants"
+
+const RestaurantCard = (props) => {
+    const {resData} = props;
+    const {cloudinaryImageId, name, cuisines, avgRating, costForTwo} = resData?.info
+    const {deliveryTime} = resData?.info.sla
+
+    return (
+      <div className="res-card" style = {{backgroundColor: "#f0f0f0"}}>
+        <img className="res-logo"
+        alt="" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId}/> 
+        <h3>{name}</h3>
+        <h4>{cuisines.join(", ")}</h4>
+        <h4>{avgRating} stars</h4>
+        <h4>{costForTwo}</h4>
+        <h4>{deliveryTime} minutes</h4>
+        
+      </div>
+    );
+  };
+
+  export default RestaurantCard
